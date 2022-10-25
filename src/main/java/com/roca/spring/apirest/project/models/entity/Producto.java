75774aff4113ab394931aed8e3929748f3800098
EdwinMendozaMinchan <emendozaminchan@gmail.com>
2022-10-25 13:23:38 -0500

    add entity product

---
+++ src/main/java/com/roca/spring/apirest/project/models/entity/Producto.java
@@ -0,0 +1,40 @@
+package com.roca.spring.apirest.project.models.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Entity
+@Table(name = "productos")
+public class Producto implements Serializable{
+		
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nombre;
+	private double precio;
+	
+	@Column(name = "create_at")
+	@Temporal(TemporalType.DATE)
+	private  Date createAt;
+	
+	public void prePersist() {
+		createAt = new Date();
+	}	
+	
+	private static final long serialVersionUID = 1L;	
+
+}
