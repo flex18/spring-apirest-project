6c56664323484b158fec63a6492f68016f424689
EdwinMendozaMinchan <emendozaminchan@gmail.com>
2022-10-25 13:02:00 -0500

    add update at controller and properties

--- src/main/java/com/roca/spring/apirest/project/controllers/ClienteController.java
+++ src/main/java/com/roca/spring/apirest/project/controllers/ClienteController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -17,7 +19,7 @@ import com.roca.spring.apirest.project.services.inter.ICliente;
 
 @RestController
 @RequestMapping("/bodega")
-@CrossOrigin(origins = "http://192.168.1.140:4200", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PATCH,
+@CrossOrigin(origins = "http://localhost:4200", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PATCH,
 	    RequestMethod.DELETE})
 public class ClienteController {
 	
@@ -30,7 +32,7 @@ public class ClienteController {
 		return clienteService.fndAll();
 	}
 	
-	@GetMapping("/clientes/id")
+	@GetMapping("/clientes/{id}")
 	public Cliente showById(@PathVariable Long id) {
 		return clienteService.findById(id);
 	}
@@ -38,6 +40,22 @@ public class ClienteController {
 	@PostMapping("/clientes")
 	public Cliente create(@RequestBody Cliente cliente) {
 		return clienteService.save(cliente);
+	}
+
+	@PutMapping("clientes/{id}")
+	public Cliente update(@RequestBody Cliente request, @PathVariable Long id){
+		Cliente oldCliente = clienteService.findById(id);
+		
+		oldCliente.setNombre(request.getNombre());
+		oldCliente.setApellido(request.getApellido());
+		oldCliente.setEmail(request.getEmail());
+		
+		return clienteService.save(request);
+	}
+	
+	@DeleteMapping("clientes/{id}")
+	public void delete(@PathVariable Long id) {
+		clienteService.delete(id);
 	}
 
 }
