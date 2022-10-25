ad543404f70b17e443aab13f4c9fa53d303faa4d
EdwinMendozaMinchan <emendozaminchan@gmail.com>
2022-10-25 14:13:30 -0500

    add methods

--- src/main/java/com/roca/spring/apirest/project/services/inter/ProductoInterface.java
+++ src/main/java/com/roca/spring/apirest/project/services/inter/ProductoInterface.java
@@ -1,6 +1,15 @@
 package com.roca.spring.apirest.project.services.inter;
 
+import java.util.List;
+
+import com.roca.spring.apirest.project.models.entity.Producto;
+
 public interface ProductoInterface {
 	
+	public List<Producto> findAll();
+	
+	public Producto insert(Producto request);
+	
+	public Producto findById(Long id);
 
 }
