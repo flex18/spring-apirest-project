4aae175462bfdf1b93bda9042aed7a6136e9c125
EdwinMendozaMinchan <emendozaminchan@gmail.com>
2022-10-27 17:15:52 -0500

    add cors conexion

--- src/main/java/com/roca/spring/apirest/project/auth/ResourceServerConfig.java
+++ src/main/java/com/roca/spring/apirest/project/auth/ResourceServerConfig.java
@@ -1,9 +1,18 @@
 package com.roca.spring.apirest.project.auth;
 
+import java.util.Arrays;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
@@ -14,9 +23,26 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 		/*.antMatchers(HttpMethod.GET, "/bodega/clientes/{id}").hasAnyRole("USER", "ADMIN")
 		.antMatchers(HttpMethod.POST, "/bodega/clientes").hasAnyRole("USER", "ADMIN")
 		.antMatchers("/bodega/clientes/**").hasAnyRole("USER", "ADMIN")*/
-		.anyRequest().authenticated();
+		.anyRequest().authenticated()
+		.and().cors().configurationSource(corsConfigurationSource());
 	}
 	
-	//.antMatchers(HttpMethod.POST, "/bodega/clientes").hasRole("ADMIN")
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST","PUT","DELETE","PATCH","OPTIONS"));
+        configuration.setAllowCredentials(true);
+        configuration.setAllowedHeaders(Arrays.asList("Content-Type", "Authorization"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+    
+    public FilterRegistrationBean<CorsFilter> corsFilter(){
+    	FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<CorsFilter>(new CorsFilter(corsConfigurationSource()));
+    	bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+    	return bean;
+    }
 	
 }
