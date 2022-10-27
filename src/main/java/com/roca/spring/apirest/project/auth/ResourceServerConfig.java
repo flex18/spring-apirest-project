a1862d73e6f90bc9415d54bf9c21ceafc2e5c028
EdwinMendozaMinchan <emendozaminchan@gmail.com>
2022-10-27 13:02:31 -0500

    add auth-jwt

---
+++ src/main/java/com/roca/spring/apirest/project/auth/ResourceServerConfig.java
@@ -0,0 +1,20 @@
+package com.roca.spring.apirest.project.auth;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+
+@Configuration
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+
+	@Override
+	public void configure(HttpSecurity http) throws Exception {
+		// TODO Auto-generated method stub
+		http.authorizeRequests().antMatchers(HttpMethod.GET, "/bodega/clientes").permitAll()
+		.anyRequest().authenticated();
+	}
+	
+	
+	
+}
