0736d430faad74f4d2f0f2ecaa11886dec7a03e7
EdwinMendozaMinchan <emendozaminchan@gmail.com>
2022-11-03 09:56:21 -0500

    add post factura

--- src/main/java/com/roca/spring/apirest/project/controllers/FacturaController.java
+++ src/main/java/com/roca/spring/apirest/project/controllers/FacturaController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -38,6 +40,12 @@ public class FacturaController {
 	@ResponseStatus(HttpStatus.OK)
 	public List<Producto> filtrarProducto(@PathVariable String term){
 		return clienteService.findProductoByNombre(term);
+	}
+	
+	@PostMapping("/facturas")
+	@ResponseStatus(value = HttpStatus.CREATED)
+	public Factura createFactura(@RequestBody Factura request) {
+		return clienteService.saveFactura(request);
 	}
 
 }
